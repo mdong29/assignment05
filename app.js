5d6b4c8976c9e154a89c276900f41789df13bad6
mdong29 <mjd20@mail.fresnostate.edu>
2017-03-30 00:04:41 -0700

    section2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express'),
-    mongoose = require('mongoose');
-bodyParser = require('body-parser');
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
 
 
 var db = mongoose.connect('mongodb://localhost/bookAPI');
@@ -14,44 +14,10 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var bookRouter = express.Router();
+bookRouter = require('./Routes/bookRoutes')(Book);
 
-bookRouter.route('/Books')
-    .post(function(req, res) {
-        var book = new Book(req.body);
 
-        console.log(book);
-        res.send(book);
-    })
-    .get(function(req, res) {
-        Book.find(function(err, books) {
-            var query = {};
-
-            if (req.query.genre) {
-                query.genre = req.query.genre;
-            }
-            Book.find(query, function(err, books) {
-                if (err)
-                    res.status(500).send(err);
-                else
-                    res.json(books);
-            });
-        });
-    });
-
-bookRouter.route('/Books/:bookId')
-    .get(function(req, res) {
-
-        Book.findById(req.params.bookId, function(err, book) {
-            if (err)
-                res.status(500).send(err);
-            else
-                res.json(book);
-        });
-    });
-
-app.use('/api', bookRouter);
-
+app.use('/api/books', bookRouter);
 
 
 app.get('/', function(req, res) {
